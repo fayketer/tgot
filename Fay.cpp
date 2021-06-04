f4b751b8f3c6969bdb033acddfb19953f1db04b0
fayketer <85346977+fayketer@users.noreply.github.com>
2021-06-04 05:42:57 -0700

    Add files via upload

---
+++ Fay.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+//program
+float p=3.14;
+float rad;
+float circle()
+{
+	float area;
+	area=rad*rad*p;
+	return(area)
+	cout <<"enter the radius"<<endl;
+	cin >>rad,
+	cout<<"the area of a circle"<<circle();
+	return 0;
+})
